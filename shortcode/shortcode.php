1c8109ae1b96e1fa95a0b4fa0ce5ad23a1344cf1
Dwarve <dwarve@gmail.com>
2015-08-30 01:20:19 +0430

    Adding disclaimer
    Legal disclaimer added to instruction shortcode.

--- shortcode/shortcode.php
+++ shortcode/shortcode.php
@@ -177,7 +177,9 @@ function lolitemset_instruction() {
             <li>Currently the processing script could be setup to be ran periodically to get more up to date data but a more advanced updating script is planned for the future.</li>
 
         </ul>
-    </div>';
+    </div>
+    <p>Item Set Generator isn\'t endorsed by Riot Games and doesn\'t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
+    ';
     return $instr;
 }
 
